600a7a469da6d6e5db655f45d3c66fa4ab059c3d
Julio Reyes <Julio.Reyes@queenslibrary.org>
2021-02-01 12:42:44 -0500

    Initial Commit

---
+++ IOCContainerFramework/IOCContainerFramework.h
@@ -0,0 +1,18 @@
+//
+//  IOCContainerFramework.h
+//  IOCContainerFramework
+//
+//  Created by Julio Reyes on 2/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOCContainerFramework.
+FOUNDATION_EXPORT double IOCContainerFrameworkVersionNumber;
+
+//! Project version string for IOCContainerFramework.
+FOUNDATION_EXPORT const unsigned char IOCContainerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOCContainerFramework/PublicHeader.h>
+
+
